ada08001cbfe1a589f1dac58086c6ff1df527913
Juan <juancho4450@gmail.com>
2024-11-07 19:27:30 -0300

    Primera parte de Gestionar Obra

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -6,8 +6,37 @@ from abc import abstractmethod
 
 
 class GestionarObra(ABC):
-    def __init__(self):
-        pass
+    
+    @classmethod
+    def extraer_datos(cls, ruta_dataset):
+        try:
+            df = pd.read_csv("observatorio_obras_urbanas.csv")
+            print("Datos extraídos con éxito.")
+            return df
+        except FileNotFoundError as e:
+            print(f"Error: El archivo {ruta_dataset} no se encuentra. Verifique la ruta.")
+            return None
+        except pd.errors.EmptyDataError as e:
+            print(f"Error: El archivo está vacío.")
+            return None
+        except pd.errors.ParserError as e:
+            print(f"Error: El archivo no puede ser analizado correctamente. Verifique el formato.")
+            return None
+        except Exception as e:
+            print(f"Error inesperado al extraer los datos: {e}")
+            return None
+
+    @classmethod
+    def conectar_db(cls):
+        try:
+            cls.db = pw.SqliteDatabase('obras_urbanas.db')
+            cls.db.connect()
+            print("Conexión a la base de datos exitosa.")
+        except pw.OperationalError as e:
+            print(f"Error de conexión a la base de datos: {e}")
+        except Exception as e:
+            print(f"Error inesperado al conectar a la base de datos: {e}")
+
     
 
 
